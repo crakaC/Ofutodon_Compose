1a93e46bf64ded2015362b09234659dbb9f9a3db
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2021-12-11 00:13:41 +0900

    Provide LocalContentAlpha 1f in Theme

--- app/src/main/java/com/crakac/ofutodon/ui/theme/Theme.kt
+++ app/src/main/java/com/crakac/ofutodon/ui/theme/Theme.kt
@@ -1,10 +1,12 @@
 package com.crakac.ofutodon.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
     primary = Blue700,
@@ -24,10 +26,12 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun OfutodonTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    MaterialTheme(
-        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalContentAlpha provides 1f) {
+        MaterialTheme(
+            colors = if (darkTheme) DarkColorPalette else LightColorPalette,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
