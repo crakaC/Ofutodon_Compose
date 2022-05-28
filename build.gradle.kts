85f262aa669329429347b8a7f99f0fc408b36527
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2022-05-28 22:35:40 +0900

    Migrate root build.gradle to KotlinScript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath libs.hilt.gradle
-        classpath libs.google.services
+        classpath(libs.hilt.gradle)
+        classpath(libs.google.services)
     }
 }
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kover)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin) apply false
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.create<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
 
-apply plugin: 'kover'
+apply(plugin = "kover")
